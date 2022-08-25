90750329b386786702f9f1ea6a13ee301c8032ff
Rafael Pauwels <rafapauwels@gmail.com>
2022-08-24 21:55:23 -0300

    Diminuição de ruído e melhoria na detecção

--- exp/stereo-match/main.py
+++ exp/stereo-match/main.py
@@ -8,8 +8,12 @@ from calibrador import Calibrador
 def main():
     path = "/home/pauwels/Documents/Sync/UFABC/PGC/pgc"
 
+    plt.ion()
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    ax.set_xlim3d(0, 800)
+    ax.set_ylim3d(0, 100)
+    ax.set_zlim3d(0, 800)
 
     cam_e = cv.VideoCapture(0)
     cam_d = cv.VideoCapture(2)
@@ -35,10 +39,12 @@ def main():
         obj_d = findCircle(frame_blur_d)
 
         cv.imshow('ESQUERDA', frame_blur_e)
-        cv.imshow('DIREITA', frame_blur_d)
+        # cv.imshow('DIREITA', frame_blur_d)
 
-        cv.imshow("filtro esq", hsv_filter(frame_ori_e))
-        cv.imshow("filtro dir", hsv_filter(frame_ori_d))
+        m = hsv_filter(frame_blur_e)
+        cv.imshow("mask", m)
+        cv.imshow("bitwise esq", cv.bitwise_and(frame_blur_e, frame_blur_e, mask=m))
+        # cv.imshow("filtro dir", hsv_filter(frame_ori_d))
 
         key = cv.waitKey(1)
         if  key & 0xFF == ord('q'):
@@ -58,18 +64,20 @@ def main():
         disparidade = x_e - x_d
         dist = abs((dist_cam * f_pixel) / disparidade)
 
-        draw3D(obj_e, dist)
+        # draw3D(obj_e[0], dist, obj_e[1], fig, ax)
 
         print(dist)
         cv.putText(frame_ori_e, str(round(dist, 3)), (250, 100), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
-def draw3D(x, y, z):
+def draw3D(x, y, z, fig, ax):
+    ax.scatter(x, y, -z)
+    plt.draw()
+    plt.pause(0.1)
+    # ax.cla()
 
-
-
-# Encontra maior circulo vermelho dentro do frame
+# Encontra maior circulo verde dentro do frame
 def findCircle(frame):
-    # Filtra cor vermelha
+    # Filtra cor verde
     mascara = hsv_filter(frame)
 
     contornos = cv.findContours(mascara.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -94,17 +102,25 @@ def findCircle(frame):
 # https://cvexplained.wordpress.com/2020/04/28/color-detection-hsv/
 def hsv_filter(frame):
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # Boundaries encontradas com hsvcheck.py    
+    green_lower = np.array([66, 117, 125])
+    green_upper = np.array([90, 255, 200])
+
+    green_mask = cv.inRange(hsv, green_lower, green_upper)
+
+    # Operacoes morpho para reduzir ruidos
+    green_mask = cv.erode(green_mask, None, iterations=2)
+    green_mask = cv.dilate(green_mask, None, iterations=2)
+
+    return green_mask
+
     # lower boundary RED color range values; Hue (0 - 10)
-    lower1 = np.array([0, 100, 20])
-    upper1 = np.array([10, 255, 255])
+    # lower1 = np.array([0, 100, 20])
+    # upper1 = np.array([10, 255, 255])
     
     # upper boundary RED color range values; Hue (160 - 180)
-    lower2 = np.array([160,50,50])
-    upper2 = np.array([179,255,255])
-
-    mask1 = cv.inRange(hsv, lower1, upper1)
-    mask2 = cv.inRange(hsv, lower2, upper2)
-
-    return mask2
+    # lower2 = np.array([160,50,50])
+    # upper2 = np.array([179,255,255])
 
 main()
